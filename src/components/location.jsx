e1749eb988a42d23bdde3f0bc6000c4f60bad899
unknown <akdl4545@syuin.ac.kr>
2025-06-03 18:26:37 +0900

    change

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,8 +47,8 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1748937489114",
-    "key" : "2wj69xfdybb",
+    "timestamp" : "1652464367301",
+    "key" : "2a8fe",
     "mapWidth" : "640",
     "mapHeight" : "360"
   }).render();`);
