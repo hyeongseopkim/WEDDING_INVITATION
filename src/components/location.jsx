b0dedacd061cc578ae5f9c82b8e2b0cfaf35b850
unknown <akdl4545@syuin.ac.kr>
2025-06-03 19:22:26 +0900

    change

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -99,7 +99,7 @@ const Location = () => {
       </Divider>
       <Image src={Flower} />
       <Map
-        id="daumRoughmapContainer1652464367301"
+        id="daumRoughmapContainer1748945449932"
         className="root_daum_roughmap root_daum_roughmap_landing"
       ></Map>
       <Content>
